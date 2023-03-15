66ba2aed0e50703eb5d6a5351a179ad2371855e3
Tony Grimes <acidtone@tonygrimes.com>
2023-03-15 11:23:33 -0600

    added end of morning code

--- src/routes/dailies/+page.svelte
+++ src/routes/dailies/+page.svelte
@@ -4,6 +4,7 @@
     <li><strong>2023-03-15-svelte-components</strong>
       <ul>
         <li><a href="/dailies/2023-03-15-svelte-components/calculator">Calculator</a></li>
+        <li><a href="/dailies/2023-03-15-svelte-components/each-example"><code>each</code> Example</a></li>
       </ul>
     </li>
   </ul>
---
+++ src/routes/dailies/2023-03-15-svelte-components/each-example/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  let fruits = ["Apple", "Orange", "Plum"];
+</script>
+<main>
+  <h1>Cordelle's Favourite Fruits</h1>
+  <ul>
+    {#each fruits as fruit}
+      <li><strong>{fruit}</strong></li>
+    {/each}
+  </ul>
+</main>
