61949907da42b9779fe168ef943454f04c250715
Tony Grimes <acidtone@tonygrimes.com>
2023-03-17 09:30:40 -0600

    added dom-elements

---
+++ src/routes/dailies/2023-03-17-svelte-events/dom-element/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  let animalId = 237;
+
+  const handleClick = function() {
+    animalId++;
+    console.log(animalId);
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div on:click={handleClick}>
+  <h3>Card Title</h3>
+  <img src="https://picsum.photos/id/{animalId}/200/200" alt="Random Lorem Picsum">
+  <p>This is a puppy?</p>
+  <p>Current ID: {animalId}</p>
+</div>
+
+<style>
+  div {
+    width: 30ch;
+    border: 1px solid black;
+    background-color: #eee;
+
+    padding: 1rem;
+    margin: auto;
+  }
+
+  img {
+    width: 100%;
+  }
+</style>
